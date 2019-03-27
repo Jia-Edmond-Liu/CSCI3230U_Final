a3dafe93b6dd98f8ddb80a719d0cf2304edcf8ef
Tehseen Chaudhry <tehseenchaudhry@Tehseens-MacBook-Pro.local>
2019-03-27 19:41:38 -0400

    Adjusted video playback settings

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-	var iframe = $('#background-video')[0],
-	player = $f(iframe),
-	status = $('.status');
+	var video = $('.background');
 
-	player.addEvent('ready', function() {
-	    player.api('setVolume', 0);
-	});
+			var player = new Vimeo.Player(video);
+
+	player.ready().then(function() {
+		 player.setVolume(0);
+		});
 	// $("#infvis").click(function(){
 	//
 	// });
