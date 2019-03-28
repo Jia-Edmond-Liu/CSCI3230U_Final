5cb7f1f214a8f69d2473a51d9181b3eaadc3b3e2
Tehseen Chaudhry <tehseenchaudhry@Tehseens-MacBook-Pro.local>
2019-03-28 16:21:31 -0400

    Adding videos through AJAX, edited JS file for portfolio

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,3 +1,5 @@
+$.support.cors = true;
+
 $(document).ready(function(){
 
 	$("#infvis").hover(function(){
@@ -31,6 +33,70 @@ $(document).ready(function(){
     $(".photoButton").css("background-color", "Transparent");
     $(".photoButton").css("color", 'white');
   });
+
+  $(".videoButton").click(function() { //scrolls to video portfolio when clicked
+    $('html,body').animate({
+        scrollTop: $(".video-portfolio").offset().top},
+        'slow');
+  });
+
+  var xhttp = new XMLHttpRequest(); //pull from JSON
+  xhttp.onreadystatechange = function(){
+    if ((xhttp.readyState == 4) && (xhttp.status)){
+
+      var data = xhttp.responseText;
+      var json = JSON.parse(data);
+      videoinfo = json['videos'];
+    }
+  }
+  xhttp.open("GET", '/videos.json', true);
+  xhttp.send();
+
+  console.log('test')
+  function pullVideos(){
+    for (var i = 0; i < videoinfo.length; i++) {
+      var title = videoinfo[i]['title'];
+      var videolink = videoinfo[i]['video']
+      var tagline = videoinfo[i]['tagline']
+      console.log('test')
+      displayVideos(title, videolink, tagline);
+    }
+  }
+
+  function  displayVideos(title, videolink, tagline) {
+    var appendthis = '<br> <iframe width="1600" height="900" src="'+videolink+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe> <h2 id="heading">'+title+'\
+    </h2> <h4 id="tagline">' + tagline+ '</h4>';
+
+    $(".jqueryvideos").append(appendthis);
+
+
+    // var video = document.createElement("IMG");
+    // var res = image.slice(0, 6);
+    // var imgname = "images/" + image.slice(7)
+    // console.log(imgname);
+    // //image1.setAttribute(res,imgname);
+    // image1.src = imgname;
+    // image1.setAttribute("width", "200");
+    // image1.setAttribute("height", "200");
+    // document.body.appendChild(image1);
+    //
+    // var hotelTitle = document.createElement("P");
+    // var hotelTitleName = document.createTextNode(name);
+    // hotelTitle.appendChild(hotelTitleName);
+    // document.body.appendChild(hotelTitle);
+    //
+    // var hotelPrice = document.createElement("P");
+    // var priceNum = document.createTextNode(price);
+    // hotelTitle.appendChild(priceNum);
+    // document.body.appendChild(hotelTitle);
+
+
+    //Connect CSS class
+    /*var att = document.createAttribute("class");
+    att.value = "democlass";
+    h1.setAttributeNode(att);*/
+  }
+
 
 
 	var video = $('.background'); // background video volume off
