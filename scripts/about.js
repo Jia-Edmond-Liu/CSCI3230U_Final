f075c76c3af087caba617b3769d4f5023fe691b9
Ed <jia.liu@uoit,net>
2019-03-27 20:25:03 -0400

    Added redirect back to main and about.js

---
+++ scripts/about.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	//Changes the page back to main
+	$("#infvis").click(function(){
+		console.log("hello");
+		window.location.href = "../FinalProject/main.html";
+	});
+});
