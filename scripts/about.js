22dc4d2737aa48706e10ed76b35e50205d65aeb1
Ed <jia.liu@uoit,net>
2019-03-27 20:54:47 -0400

    change cursor on hover, added shop, fixed spacing

--- scripts/about.js
+++ scripts/about.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 	//Changes the page back to main
+	$("#infvis").hover(function(){
+		$("#infvis").css("cursor","pointer");
+	});
+	$("#about").hover(function(){
+		$("#about").css("cursor","pointer");
+	});
+	$("#portfolio").hover(function(){
+		$("#portfolio").css("cursor","pointer");
+	});
+	$("#contact").hover(function(){
+		$("#contact").css("cursor","pointer");
+	});
+	$("#shop").hover(function(){
+		$("#shop").css("cursor","pointer");
+	});
 	$("#infvis").click(function(){
 		console.log("hello");
 		window.location.href = "../FinalProject/main.html";
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-
+	
+	$("#infvis").hover(function(){
+		$("#infvis").css("cursor","pointer");
+	});
+	$("#about").hover(function(){
+		$("#about").css("cursor","pointer");
+	});
+	$("#portfolio").hover(function(){
+		$("#portfolio").css("cursor","pointer");
+	});
+	$("#contact").hover(function(){
+		$("#contact").css("cursor","pointer");
+	});
+	$("#shop").hover(function(){
+		$("#shop").css("cursor","pointer");
+	});
 	var video = $('.background');
 
 			var player = new Vimeo.Player(video);
